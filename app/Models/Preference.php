7259fd88275f68bd263160c73c0bc7e225144162
cmunhozprog03 <cmunhozprog03@hotmail.com>
2022-04-07 11:39:49 -0300

    many-to-may and many-to-many-pivot

--- app/Models/Preference.php
+++ app/Models/Preference.php
@@ -16,5 +16,6 @@ class Preference extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,51 @@
 
 use App\Models\{
     Course,
+    Permission,
     Preference,
     User
 };
 use Illuminate\Support\Facades\Route;
+
+Route::get('/many-to-many-pivot', function(){
+    $user = User::with('permissions')->find(1);
+    //dd($user->érmissions);
+
+    $user->permissions()->attach([
+        1 => ['active' => false],
+        3 => ['active' => false]
+    ]);
+
+    $user->refresh();
+
+    echo "{$user->name} <br>";
+    foreach ($user->permissions as $permission) {
+        echo "{$permission->name} - {$permission->pivot->active} <br>";
+    }
+});
+
+Route::get('/many-to-many', function(){
+
+    //dd(Permission::create(['name' => 'menu_03']));
+
+    $user = User::with('permissions')->find(1);
+
+    //$permission = Permission::find(1);
+    //$user->permissions()->save($permission);
+    // $user->permissions()->saveMany([
+    //     Permission::find(1),
+    //     Permission::find(2),
+    //     Permission::find(3),
+    // ]);
+
+    //$user->permissions()->sync([2]);
+    //$user->permissions()->attach([1,3]);
+    $user->permissions()->detach([1,3]);
+
+    $user->refresh();
+
+    dd($user->permissions);
+});
 
 Route::get('/one-to-many', function(){
     //$course = Course::create(['name' => 'Curso Laravel']);
